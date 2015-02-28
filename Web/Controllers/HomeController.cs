72a6133691f1d68f6e517507f94f8ebbdac6923a
Bo Flynn <boflynn@gmail.com>
2015-02-28 14:52:42 -0500

    Added basic download form and controller methods to view/download files.

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
 
 namespace SimpleProxy.Web.Controllers
 {
@@ -14,6 +18,42 @@ namespace SimpleProxy.Web.Controllers
             ViewBag.Message = "Your application description page.";
 
             return View();
+        }
+
+        public ActionResult DownloadFile(string url)
+        {
+            var fileName = GetFileNameFromUrl(url);
+            var data = DownloadUrl(url);
+            var mimeType = MimeMapping.GetMimeMapping(fileName);
+
+            return File(data, mimeType, fileName);
+        }
+
+        public ActionResult ViewFile(string url)
+        {
+            var fileName = GetFileNameFromUrl(url);
+            var data = DownloadUrl(url);
+            var mimeType = MimeMapping.GetMimeMapping(fileName);
+            var stream = new MemoryStream(data);
+
+            return new FileStreamResult(stream, mimeType);
+        }
+
+        private string GetFileNameFromUrl(string url)
+        {
+            return url.Substring(url.LastIndexOf("/", StringComparison.Ordinal) + 1);
+        }
+
+        private byte[] DownloadUrl(string url)
+        {
+            byte[] data;
+
+            using (var client = new WebClient())
+            {
+                data = client.DownloadData(url);
+            }
+
+            return data;
         }
     }
 }
